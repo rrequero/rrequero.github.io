a3ed0c049284e9005fcb7e99ee9f00cf93215e04
Ra <rrequero@gmail.com>
2019-05-26 23:31:07 +0200

    fix js

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,11 @@ require('../sass/main.scss');
 
 function init() {
 
-    document.querySelector('.menu-toggle').addEventListener('click', function () {
+    document.querySelector('.menu-toggle').addEventListener('click', function (event) {
         document.body.classList.toggle('menu-open');
     });
-    document.querySelector('main').addEventListener('click', function () {
-        document.body.classList.toggle('menu-open');
+    document.querySelector('main').addEventListener('click', function (event) {
+        document.body.classList.remove('menu-open');
     });
 }
 
