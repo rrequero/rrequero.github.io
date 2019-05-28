c27d429b4ef74d4e2839188077153d84c969ae28
Ra <rrequero@gmail.com>
2019-05-28 16:13:05 +0200

    remove menu

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,12 @@ require('../sass/main.scss');
 
 function init() {
 
-    document.querySelector('.menu-toggle').addEventListener('click', function (event) {
-        document.body.classList.toggle('menu-open');
-    });
-    document.querySelector('main').addEventListener('click', function (event) {
-        document.body.classList.remove('menu-open');
-    });
+    // document.querySelector('.menu-toggle').addEventListener('click', function (event) {
+    //     document.body.classList.toggle('menu-open');
+    // });
+    // document.querySelector('main').addEventListener('click', function (event) {
+    //     document.body.classList.remove('menu-open');
+    // });
 }
 
 window.onload = init;
